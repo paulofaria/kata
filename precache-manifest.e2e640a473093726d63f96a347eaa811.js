607d7a5d5744c4cf870c1f8865c1413133e1a816
Paulo Faria <paulo.faria.rl@gmail.com>
2020-04-24 23:47:53 -0300

    Updates

---
+++ precache-manifest.e2e640a473093726d63f96a347eaa811.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c80e9d61ce7eebd968e75aa439d00d92",
+    "url": "/kata/index.html"
+  },
+  {
+    "revision": "c21c421068bf2a3dc64f",
+    "url": "/kata/static/css/2.89f067aa.chunk.css"
+  },
+  {
+    "revision": "03e4fab37bd14c43ee08",
+    "url": "/kata/static/css/main.dad3c075.chunk.css"
+  },
+  {
+    "revision": "c21c421068bf2a3dc64f",
+    "url": "/kata/static/js/2.1bcc2e1e.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/kata/static/js/2.1bcc2e1e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "03e4fab37bd14c43ee08",
+    "url": "/kata/static/js/main.a007f8a1.chunk.js"
+  },
+  {
+    "revision": "2bbeb4e42c54f01bebe2",
+    "url": "/kata/static/js/runtime-main.832ba453.js"
+  }
+]);
